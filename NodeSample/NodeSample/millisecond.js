73c50b33b71f6e650cef986ab2f3c3a95b5b3a21
GamSaHam <cleverones@naver.com>
2016-12-01 16:37:01 +0900

    calculate delay time

---
+++ NodeSample/NodeSample/millisecond.js
@@ -0,0 +1,17 @@
+﻿
+console.time('timeCheck');
+
+var value = 1;
+for (var i = 0; i < 10000000; i++)
+{
+    for (var i = 0; i < 10000000; i++) {
+        for (var i = 0; i < 10000000; i++) {
+            value = value * i;
+        }
+    }
+}
+
+console.timeEnd('timeCheck');
+
+
+console.log('test');
